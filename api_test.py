dfcd4bdf4b82c68c3b9650d0e2da9182565c3808
areyoureadthemanual <areyoureadthemanual@gmail.com>
2018-10-07 15:28:46 +0500

    Add cases to business logic.

--- api_test.py
+++ api_test.py
@@ -1,14 +1,17 @@
-import requests
-import json
 import base64
+import json
+import time
+
+import requests
 
 
 def authentication():
-    r = requests.post('http://188.18.13.78:9000/api/m/account/dev',
-                      data={"devType": "A", "pushToken": "firebaseSecretToken", "brand": 'YIT'}
-                      )
+    url = 'http://188.18.13.78:9000/api/m/account/dev'
+    data = {"devType": "A", "pushToken": "firebaseSecretToken", "brand": 'YIT'}
 
-    # Лучше упасть с ошибкой сейчас, чем что-нибудь отвалится потом из-за отсутствующего токена
+    r = requests.post(url, data=data)
+
+    # Лучше упасть с ошибкой сейчас, если код ответа не 200, чем что-нибудь отвалится потом из-за отсутствующего токена
     r.raise_for_status()
 
     response_content = json.loads(r.text)
@@ -18,41 +21,75 @@ def authentication():
     return token_b64, str(r.status_code)
 
 
-def set_phone_number():
+def set_phone_number(test=False):
     token_b64 = authentication()[0]
-    r = requests.post('http://188.18.13.78:9000/api/m/account/phone',
-                      headers={'Authorization': 'basic ' + token_b64.decode("utf-8")},
-                      data={"phone": "9125212573"}
-                      )
+    url = 'http://188.18.13.78:9000/api/m/account/phone'
+    headers = {'Authorization': 'basic ' + token_b64.decode("utf-8")}
+    data = {"phone": "9125212573"}
 
-    error_code = json.loads(r.text)['errorCode']
+    r = requests.post(url, headers=headers, data=data)
 
-    return str(r.status_code), str(error_code)
+    r.raise_for_status()
+
+    if test:
+        # Кейс с верным ответом, без таймаута
+        time.sleep(200)
+        r = requests.post(url, headers=headers, data=data)
+
+        r.raise_for_status()
+        error_code = str(json.loads(r.text)['errorCode'])
+        assert error_code == '1', 'Function "set_phone_number" returns wrong error code: ' + error_code
+
+        # Кейс с таймаутом (повторный запрос в течение 180 секунд)
+        r = requests.post(url, headers=headers, data=data)
+        r = requests.post(url, headers=headers, data=data)
+
+        r.raise_for_status()
+        error_code = str(json.loads(r.text)['errorCode'])
+        assert error_code == '2', 'Function "set_phone_number" returns wrong error code: ' + error_code
+
+    return str(r.status_code)
 
 
-def confirm_phone_number():
+def confirm_phone_number(test=False):
+    set_phone_number()
     token_b64 = authentication()[0]
-    r = requests.post('http://188.18.13.78:9000/api/m/account/phone/confirm',
-                      headers={'Authorization': 'basic ' + token_b64.decode("utf-8")},
-                      data={"phone": "9125212573", "code": "12345"}
-                      )
+    url = 'http://188.18.13.78:9000/api/m/account/phone/confirm'
+    headers = {'Authorization': 'basic ' + token_b64.decode("utf-8")}
+    data = {"phone": "9125212573", "code": "12345"}
 
-    error_code = json.loads(r.text)['errorCode']
-    return str(r.status_code), str(error_code)
+    # Кейс с верным ответом, без таймаута
+    r = requests.post(url, headers=headers, data=data)
+
+    r.raise_for_status()
+    error_code = str(json.loads(r.text)['errorCode'])
+    assert error_code == '1', 'Function "confirm_phone_number" returns wrong error code: ' + error_code
+
+    if test:
+        # Кейс с истекшим кодом подтверждения (больше 180 секунд)
+        time.sleep(200)
+        r = requests.post(url, headers=headers, data=data)
+
+        r.raise_for_status()
+        error_code = str(json.loads(r.text)['errorCode'])
+        assert error_code == '2', 'Function "confirm_phone_number" returns wrong error code: ' + error_code
+        # Кейс с неправильным кодом подтверждения
+        set_phone_number()
+        data = {"phone": "9125212573", "code": "wrong_code"}
+        r = requests.post(url, headers=headers, data=data)
+
+        r.raise_for_status()
+        error_code = str(json.loads(r.text)['errorCode'])
+        assert error_code == '3', 'Function "confirm_phone_number" returns wrong error code: ' + error_code
+
+    return str(r.status_code)
 
 
 test_authentication = authentication()
-assert test_authentication[1] == '200', 'Func "authentication" returns status code:' + test_authentication[1]
+test_set_phone_number = set_phone_number(test=True)
+test_confirm_phone_number = confirm_phone_number(test=True)
 
-test_set_phone_number = set_phone_number()
-assert test_set_phone_number[0] == '200', 'Func "set_phone_number" returns status code:' + test_set_phone_number[0]
-assert test_set_phone_number[1] in ['1', '2'], 'Func "set_phone_number" returns unknown error code:' + test_set_phone_number[1]
-
-test_confirm_phone_number = confirm_phone_number()
-assert test_confirm_phone_number[0] == '200', 'Func "confirm_phone_number" returns status code:' + test_confirm_phone_number[0]
-assert test_confirm_phone_number[1] in ['1', '2', '3'], 'Func "confirm_phone_number" returns unknown error code:' + test_confirm_phone_number[1]
-
-print('Test "authentication" returns status code: ' + test_authentication[1] + '. \n',
-      'Test "set_phone_number" returns status code: ' + test_set_phone_number[0] + ', error code: '+ test_set_phone_number[1] + '. \n',
-      'Test "confirm_phone_number" returns status code: ' + test_confirm_phone_number[0] + ', error code: ' + test_confirm_phone_number[1] + '. \n',
+print('Test "authentication" returns status code: ' + test_authentication[1] + ' and valid error code "1". \n',
+      'Test "set_phone_number" returns status code: ' + test_set_phone_number[0] + ' and valid error codes: "1", "2". \n',
+      'Test "confirm_phone_number" returns status code: ' + test_confirm_phone_number[0] + ' and valid error codes: "1", "2", "3".\n',
       )
